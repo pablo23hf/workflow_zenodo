fc433a3876dc6a3d8abe44af52897d9d8d26cb83
Pablo Herreros <pablo23hf@gmail.com>
2023-02-18 12:38:24 +0100

    script

---
+++ plot.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 18 10:43:27 2023
+
+@author: pablo
+"""
+
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv(sys.argv[1]).sort_values(by='PT',ascending= False)"
+
+plt.ylim([65, 85])
+plt.bar(data['Equipo'], data['PT'])
+
+plt.xticks(data['Equipo'], rotation=90)
+
